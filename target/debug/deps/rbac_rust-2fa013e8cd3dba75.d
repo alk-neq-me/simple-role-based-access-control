125062dfd02521e043ead4498cced355ea472975
marco <toyko2001@gmail.com>
2023-10-10 17:25:15 +0630

    prototype: rust -> remove target

--- target/debug/deps/rbac_rust-2fa013e8cd3dba75.d
+++
@@ -1,9 +0,0 @@
-/home/marco/marco/projects/simple-role-based-access-control-python/target/debug/deps/rbac_rust-2fa013e8cd3dba75: rbac_rust/src/lib.rs rbac_rust/src/common.rs rbac_rust/src/rbac.rs rbac_rust/src/permissions/mod.rs rbac_rust/src/permissions/dashboard.rs
-
-/home/marco/marco/projects/simple-role-based-access-control-python/target/debug/deps/rbac_rust-2fa013e8cd3dba75.d: rbac_rust/src/lib.rs rbac_rust/src/common.rs rbac_rust/src/rbac.rs rbac_rust/src/permissions/mod.rs rbac_rust/src/permissions/dashboard.rs
-
-rbac_rust/src/lib.rs:
-rbac_rust/src/common.rs:
-rbac_rust/src/rbac.rs:
-rbac_rust/src/permissions/mod.rs:
-rbac_rust/src/permissions/dashboard.rs:
--- target/debug/deps/rbac_rust-b3b1f729d99a2a1d.d
+++
@@ -1,9 +0,0 @@
-/home/marco/marco/projects/simple-role-based-access-control-python/target/debug/deps/rbac_rust-b3b1f729d99a2a1d.rmeta: rbac_rust/src/lib.rs rbac_rust/src/common.rs rbac_rust/src/rbac.rs rbac_rust/src/permissions/mod.rs rbac_rust/src/permissions/dashboard.rs
-
-/home/marco/marco/projects/simple-role-based-access-control-python/target/debug/deps/rbac_rust-b3b1f729d99a2a1d.d: rbac_rust/src/lib.rs rbac_rust/src/common.rs rbac_rust/src/rbac.rs rbac_rust/src/permissions/mod.rs rbac_rust/src/permissions/dashboard.rs
-
-rbac_rust/src/lib.rs:
-rbac_rust/src/common.rs:
-rbac_rust/src/rbac.rs:
-rbac_rust/src/permissions/mod.rs:
-rbac_rust/src/permissions/dashboard.rs:
